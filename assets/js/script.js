8dc2857f60bf5fe68a2b12efd58f63022b9628d7
Archie9010 <archie9010@protonmail.com>
2022-07-06 13:08:22 +0000

    add question bank

---
+++ assets/js/script.js
@@ -0,0 +1,53 @@
+// Question bank 
+var questionBank= [
+    {
+        question : 'In what type of matter are atoms most tightly packed?',
+        option : ['Liquids','Solids','Gases','Plasma'],
+        answer : 'Solids'
+    },
+    {
+        question : 'What is the hottest planet in the solar system?',
+        option : ['Mercury','Saturn','Venus','Mars'],
+        answer : 'Venus'
+    },
+    {
+        question : 'Which of Newton Laws states that for every action, there is an equal and opposite reaction?',
+        option : ['The first law of motion','The second law of motion','The third law of motion','The fourth law of motion'],
+        answer : 'The third law of motion'
+    },
+    {
+        question : 'What is the nearest planet to the sun?',
+        option : ['Mercury','Earth','Pluto','Mars'],
+        answer : 'Mercury'
+    },
+    {
+        question : 'Iceland diverted roads to avoid disturbing communities of what?',
+        option : ['Dwarfs','Goblins','Elves','Fairys'],
+        answer : 'Elves'
+    },
+    {
+        question : 'How long is New Zealand’s Ninety Mile Beach?',
+        option : ['95','75','65','55'],
+        answer : '55'
+    },
+    {
+        question : 'In the state of Georgia, it’s illegal to eat what with a fork?',
+        option : ['Fish','Fried Chicken','Turkey','Pork'],
+        answer : 'Fried Chicken'
+    },
+    {
+        question : 'What is the loudest animal on Earth?',
+        option : ['Elephant','Sperm Whale','Hippo','Rooster'],
+        answer : 'Sperm Whale'
+    },
+    {
+        question : 'How many legs does a spider have?',
+        option : ['4','6','8','10'],
+        answer : '8'
+    },
+    {
+        question : 'How long do elephant pregnancies last?',
+        option : ['18 Months','20 Months','22 Months','24 Months'],
+        answer : '22 Months'
+    },
+]
